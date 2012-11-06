9bfad24dba08ea42ccf988959e75d9785ff94746
Tommi Kotilainen <tommi.kotilainen@helsinki.fi>
2012-11-06 21:30:27 +0200

    Ensimmaiset java-luokat lisatty ja naille joitain JUnit-testeja, alustava luokkakaavio lisatty ja tuntikirjanpito paivitetty

--- MuistiPeli/src/PeliLogiikka/Pelaaja.java
+++ MuistiPeli/src/PeliLogiikka/Pelaaja.java
@@ -1,13 +1,36 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package PeliLogiikka;
 
-/**
- *
- * @author Tommi
- */
 public class Pelaaja {
-    
+    private String Nimi;
+    private int pisteet;
+
+  /** Luokan konstruktori, vaatii nimen parametrina.
+   * 
+   * @param nimi 
+   */
+  public Pelaaja(String nimi){ 
+      this.Nimi = nimi;         
+      this.pisteet = 0; /** Pelaajan pisteet alkavat nollasta*/
+  }
+
+  /** Metodi palauttaa pelaajan nimen.
+   * 
+   * @return 
+   */
+  public String getNimi(){
+      return this.Nimi;
+  }
+  
+  /**Metodi kasvattaa pelaajan pisteitä yhdellä.*/
+  public void kasvataPisteita(){ 
+      this.pisteet = pisteet + 1;
+  }
+
+  /** Metodi palauttaa pelajaan pisteet.
+   * 
+   * @return 
+   */
+  public int getPisteet(){
+      return this.pisteet;
+  }
 }
